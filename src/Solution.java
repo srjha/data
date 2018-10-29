6b3431f0e3f844891be7c3c0f104569369d85bc0
easnrha <shashi.ranjan.sarveswar.jha@ericsson.com>
2018-10-29 12:21:05 +0100

    Voting problem soln

--- src/Solution.java
+++ src/Solution.java
@@ -5,6 +5,22 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        Map<String, Integer> voteMap = new TreeMap<>(Collections.reverseOrder());
+
+        for (int i = 0; i < args.length; i++) {
+
+            if (voteMap.containsKey(args[i])) {
+                voteMap.put(args[i], voteMap.get(args[i]) + 1);
+            } else {
+                voteMap.put(args[i], 1);
+            }
+        }
+
+
+       String winner  = Collections.max(voteMap.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+        System.out.println(winner);
+
 
     }
 }
